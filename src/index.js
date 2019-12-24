91e7bea28163848527da4761688b073235fbebf3
Pedro Mendonca <peumendonca@gmail.com>
2019-12-24 01:10:39 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
+const { join } = require('path')
+
+let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      webSecurity: false,
+    }
+  })
+
+  mainWindow.loadFile(join('./views/index.html'))
+
+  const isDev = process.argv.map(arg => arg === '--dev' || arg === '-d').reduce((a, b) => a || b)
+  if(isDev) {
+    mainWindow.webContents.openDevTools()
+  }
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
---
+++ src/mermaid.js
@@ -0,0 +1,55 @@
+function injectMermaid(monaco) {
+  // Register a new language
+  monaco.languages.register({ id: 'mermaid' });
+
+  // Register a tokens provider for the language
+  monaco.languages.setMonarchTokensProvider('mermaid', {
+    tokenizer: {
+      root: [
+        [/^graph .*$/, "keyword"],
+        [/\[error.*/, "custom-error"],
+        [/\[notice.*/, "custom-notice"],
+        [/\[info.*/, "custom-info"],
+        [/\[[a-zA-Z 0-9:]+\]/, "custom-date"],
+      ]
+    }
+  });
+
+  // Define a new theme that contains only rules that match this language
+  monaco.editor.defineTheme('myCoolTheme', {
+    base: 'vs',
+    inherit: false,
+    rules: [
+      { token: 'graph-definition', foreground:'#000060' },
+      { token: 'custom-info', foreground: '808080' },
+      { token: 'custom-error', foreground: 'ff0000', fontStyle: 'bold' },
+      { token: 'custom-notice', foreground: 'FFA500' },
+      { token: 'custom-date', foreground: '008800' },
+    ]
+  });
+
+  // Register a completion item provider for the new language
+  monaco.languages.registerCompletionItemProvider('mermaid', {
+    provideCompletionItems: () => {
+      var suggestions = [{
+        label: 'simpleFlowChart',
+        kind: monaco.languages.CompletionItemKind.Text,
+        insertText: `graph TD \n\tA[Label] --> B`
+      }, {
+        label: 'simpleLine',
+        kind: monaco.languages.CompletionItemKind.Snippet,
+        insertText: [
+          '${1:id1}[${2:label1}] --> ${3:id2}[${4:label2}]'
+        ].join('\n'),
+        insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+        documentation: 'Simple '
+      }];
+      return { suggestions: suggestions };
+    }
+  });
+
+}
+
+module.exports = {
+  injectMermaid
+}
